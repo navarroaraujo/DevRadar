e6cfff6d9d462ff5b23fa88b5fa038807dda56c7
Vitor N. Araújo <navarroaraujo@outlook.com>
2020-03-10 19:20:35 -0300

    feat: web app.js workflow and css improvements

--- web/src/App.js
+++ web/src/App.js
@@ -1,33 +1,85 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit= {handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+            name="github_username" 
+            id="github_username"
+            required
+            value={github_username}
+            onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+            name="techs" 
+            id="techs" 
+            required 
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input 
+              type="number" 
+              name="latitude" 
+              id="latitude" 
+              required 
+              value={latitude}
+              onChange={e => setLatitude(e.target.value)}
+              />
             </div>
-            <div class="input-block">
-              <label htmlFor="longitude">Latitude</label>
-              <input name="longitude" id="longitude" required />
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+              type="number" 
+              name="longitude" 
+              id="longitude" 
+              required value={longitude}
+              onChange={e => setLongitude(e.target.value)}
+              />
             </div>            
           </div>
 
@@ -36,7 +88,52 @@ function App() {
         </form>
       </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/25770699?s=460&v=4" alt="Navarro Araujo"/>
+              <div className="user-info">
+                <strong>Navarro Araujo</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/navarroaraujo">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/25770699?s=460&v=4" alt="Navarro Araujo"/>
+              <div className="user-info">
+                <strong>Navarro Araujo</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/navarroaraujo">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/25770699?s=460&v=4" alt="Navarro Araujo"/>
+              <div className="user-info">
+                <strong>Navarro Araujo</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/navarroaraujo">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/25770699?s=460&v=4" alt="Navarro Araujo"/>
+              <div className="user-info">
+                <strong>Navarro Araujo</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/navarroaraujo">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
