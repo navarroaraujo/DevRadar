ebbf45605d549aac4903f4f20f481dbc4f8dec51
Vitor <navarroaraujo@outlook.com>
2020-03-26 19:09:50 -0300

    Bug Fixes
    - Fixed database access
    - Add 'lodash' package

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://navarroaraujo:rf3FjMFaGB99Pwc@cluster0-kas56.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://navarroaraujo:rf3FjMFaGB99Pwc@cluster0-kas56.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
