31544d5aee60f9a2f74c53f673bb3c8caf955e08
Vitor <navarroaraujo@outlook.com>
2020-03-27 17:21:02 -0300

    WebSocket added to project
    WebSocket added so that the server can have a persistent connection with the frontend showing news devs in the front withou need of new requests to backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http')
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb://navarroaraujo:rf3FjMFaGB99Pwc@cluster0-shard-00-00-kas56.mongodb.net:27017,cluster0-shard-00-01-kas56.mongodb.net:27017,cluster0-shard-00-02-kas56.mongodb.net:27017/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true', {
     useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
